b140de634365eebc25fd365cafbe60097f38582e
Greg7519 <greg.angeloppulos@outlook.com>
2025-03-30 23:55:57 +0300

    fixed preloader not covering whole screen

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -1,17 +1,26 @@
 document.body.style.overflowY = 'hidden';
-window.onload = function(){
-        setTimeout(()=>{
-                this.document.getElementsByClassName("preloader")[0].classList.add(
-                        "goUp"
-                );
-                document.body.style.overflowY = 'visible';
+if(window.scrollY!=0){
+        document.body.style.overflowY = 'visible';
+        this.document.getElementsByClassName("preloader")[0].style.display = "none";
+        this.document.getElementsByClassName("preloader")[0].classList.add(
+                "goUp"
+        );
+}
+else{
+        window.onload = function(){
                 setTimeout(()=>{
-                        this.document.getElementsByClassName("preloader")[0].style.display = "none";
+                        this.document.getElementsByClassName("preloader")[0].classList.add(
+                                "goUp"
+                        );
+                        document.body.style.overflowY = 'visible';
+                        setTimeout(()=>{
+                                this.document.getElementsByClassName("preloader")[0].style.display = "none";
+                        },1000)
+        
                 },1000)
-
-        },1000)
-       
-     
-    
-   
+               
+             
+            
+           
+        }
 }
