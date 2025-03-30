6b4951016148dd6f0e0425f1f3304650ad6974a4
Greg7519 <greg.angeloppulos@outlook.com>
2025-03-31 00:28:04 +0300

    added some responsive text

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -1,12 +1,5 @@
 document.body.style.overflowY = 'hidden';
-if(window.scrollY!=0){
-        document.body.style.overflowY = 'visible';
-        this.document.getElementsByClassName("preloader")[0].style.display = "none";
-        this.document.getElementsByClassName("preloader")[0].classList.add(
-                "goUp"
-        );
-}
-else{
+
         window.onload = function(){
                 setTimeout(()=>{
                         this.document.getElementsByClassName("preloader")[0].classList.add(
@@ -23,4 +16,4 @@ else{
             
            
         }
-}
+
