6cbd463f8234d827b9cf9e7f232fd3955df73771
Greg7519 <greg.angeloppulos@outlook.com>
2025-04-01 00:11:17 +0300

    paused export of a function due to it stopping working

--- assets/scripts/typewrite.js
+++ assets/scripts/typewrite.js
@@ -1,6 +1,6 @@
 var i =0;
 var speed = 50;
-export function typewriter(text, element){
+function typewriter(text, element){
     if(i<text.length){
         element.innerHTML += text.charAt(i);
         i++;
@@ -16,3 +16,14 @@ window.onload = () =>{
 setTimeout(()=>{
     typewriter("Software engineer, Passionate about technology", document.getElementById("headerEng"))
 },1000)
+
+// export function typewriter(text, element){
+//     if(i<text.length){
+//         element.innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(() => {typewriter(text,element)}, speed);
+//     }
+    
+  
+   
+// }
