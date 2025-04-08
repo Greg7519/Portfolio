e78da2ef66d8149cfa0b26de46d5d54a76b69fab
Greg7519 <greg.angeloppulos@outlook.com>
2025-04-08 15:02:34 +0300

    finished frontend contact form

--- assets/scripts/handleChanges.js
+++ assets/scripts/handleChanges.js
@@ -1,4 +1,5 @@
 const ContactFormIcon = document.getElementById("activateForm");
+const hideBtn = document.getElementById("backIcon");
 const formTag = document.getElementById("form");
 const githubIcon = document.getElementById("githubIcon");
 const footer = document.getElementById("footer");
@@ -6,6 +7,9 @@ const contactForm = document.getElementById("ContactForm");
 ContactFormIcon.onclick = () =>{
     contactForm.style.display = "block";
     formTag.style.display = "flex";
+    formTag.style.animation = "appear";
+    formTag.style.animationDuration = "1s";
+    formTag.style.animationFillMode = "forwards";
 
 }
 function addAnim(elem, anim){
@@ -17,5 +21,22 @@ function addAnim(elem, anim){
     }
   
 }
+
+window.addEventListener("keydown", (event)=>{
+    if(event.key == "Escape"){
+        formTag.style.animation = "disappear";
+        formTag.style.animationDuration = "1s";
+        formTag.style.animationFillMode = "forwards";
+    }
+   
+})
+hideBtn.onclick = ()=>{
+    
+    formTag.style.animation = "disappear";
+    formTag.style.animationDuration = "1s";
+    formTag.style.animationFillMode = "forwards";
+}
+
 addAnim(ContactFormIcon, "fa-bounce");
 addAnim(githubIcon, "fa-bounce");
+addAnim(hideBtn, "fa-bounce");
